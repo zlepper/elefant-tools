6fbe8d99316c3df68558282027b4607b6914ccfb
zlepper <hansen13579@gmail.com>
2024-12-28 18:04:44 +0100

    Handle 'char' type

--- elefant-client/src/postgres_client/data_types.rs
+++ elefant-client/src/postgres_client/data_types.rs
@@ -87,6 +87,7 @@ impl_number!(i64, "int8", 20);
 impl_number!(f32, "float4", 700);
 impl_number!(f64, "float8", 701);
 
+
 impl<'a, T> FromSql<'a> for Option<T>
 where T : FromSql<'a>
 {
@@ -119,6 +120,35 @@ impl<T> ToSql for Option<T>
 
     fn is_null(&self) -> bool {
         self.is_none()
+    }
+}
+
+impl<'a> FromSql<'a> for char {
+    fn from_sql_binary(raw: &'a [u8], field: &FieldDescription) -> Result<Self, Box<dyn Error + Sync + Send>> {
+        if raw.len() != 1 {
+            return Err(format!("Invalid length for char. Expected 1 byte, got {} bytes instead. Error occurred when parsing field {:?}", raw.len(), field).into());
+        }
+        
+        Ok(raw[0] as char)
+    }
+
+    fn from_sql_text(raw: &'a str, field: &FieldDescription) -> Result<Self, Box<dyn Error + Sync + Send>> {
+        if raw.len() != 1 {
+            return Err(format!("Invalid length for char. Expected 1 byte, got {} bytes instead. Error occurred when parsing field {:?}", raw.len(), field).into());
+        }
+
+        Ok(raw.chars().next().unwrap())
+    }
+
+    fn accepts(field: &FieldDescription) -> bool {
+        field.data_type_oid == 18
+    }
+}
+
+impl ToSql for char {
+    fn to_sql_binary(&self, target_buffer: &mut Vec<u8>) -> Result<(), Box<dyn Error + Sync + Send>> {
+        target_buffer.push(*self as u8);
+        Ok(())
     }
 }
 
@@ -210,6 +240,7 @@ mod tests {
             test_integer_values!(i32);
             test_integer_values!(i64);
 
+
             helper.test_read_special_cast(1i16, "smallint").await;
 
             macro_rules! test_float_values {
@@ -240,6 +271,12 @@ mod tests {
 
             test_float_values!(f32);
             test_float_values!(f64);
+
+            let c: char = helper.client.read_single_value("select 'a'::\"char\";", &[]).await.unwrap();
+            assert_eq!(c, 'a');
+
+            let c: char = helper.client.read_single_value("select $1::\"char\";", &[&'A']).await.unwrap();
+            assert_eq!(c, 'A');
         }
 
         #[test]
